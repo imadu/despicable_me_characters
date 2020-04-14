ccadb1052c08a0be3ce9f2e271222af8e5fcf235
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2020-04-14 13:44:21 +0100

    Added more functionality to the character listing screen

--- lib/pages/characterDetailScreen.dart
+++ lib/pages/characterDetailScreen.dart
@@ -1,9 +1,13 @@
+import 'package:after_layout/after_layout.dart';
 import 'package:despicable_me/models/characters.dart';
 import 'package:despicable_me/styleguide.dart';
 import 'package:flutter/material.dart';
 
 class CharacterDetailScreen extends StatefulWidget {
   final Character character;
+  final double _expandedBottomSheetBottom = 0;
+  final double _collapsedBottomSheetBottom = -250;
+  final double _completeCollapsedBottomSheetBottom = -330;
 
   const CharacterDetailScreen({Key key, this.character}) : super(key: key);
 
@@ -11,7 +15,11 @@ class CharacterDetailScreen extends StatefulWidget {
   _CharacterDetailScreenState createState() => _CharacterDetailScreenState();
 }
 
-class _CharacterDetailScreenState extends State<CharacterDetailScreen> {
+class _CharacterDetailScreenState extends State<CharacterDetailScreen>
+    with AfterLayoutMixin<CharacterDetailScreen> {
+  double _bottomSheetBottomPosition = -330;
+  bool isCollapsed = false;
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -41,6 +49,10 @@ class _CharacterDetailScreenState extends State<CharacterDetailScreen> {
                       iconSize: 40,
                       color: Colors.white.withOpacity(0.9),
                       onPressed: () {
+                        setState(() {
+                          _bottomSheetBottomPosition =
+                              widget._completeCollapsedBottomSheetBottom;
+                        });
                         Navigator.pop(context);
                       },
                     ),
@@ -72,9 +84,115 @@ class _CharacterDetailScreenState extends State<CharacterDetailScreen> {
                     ),
                   )
                 ]),
-          )
+          ),
+          AnimatedPositioned(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.decelerate,
+              bottom: _bottomSheetBottomPosition,
+              left: 0,
+              right: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40.0),
+                        topRight: Radius.circular(40.0))),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      InkWell(
+                        onTap: _onTap,
+                        child: Container(
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                          height: 80,
+                          child: Text(
+                            "Clips",
+                            style: AppTheme.subHeading
+                                .copyWith(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: _clipsWidget())
+                    ]),
+              ))
         ],
       ),
     );
+  }
+
+  Widget _clipsWidget() {
+    return Container(
+      height: 250,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.redAccent),
+              SizedBox(height: 20),
+              roundedContainer(Colors.greenAccent),
+            ],
+          ),
+          SizedBox(width: 16),
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.orangeAccent),
+              SizedBox(height: 20),
+              roundedContainer(Colors.purple),
+            ],
+          ),
+          SizedBox(width: 16),
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.grey),
+              SizedBox(height: 20),
+              roundedContainer(Colors.blueGrey),
+            ],
+          ),
+          SizedBox(width: 16),
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.lightGreenAccent),
+              SizedBox(height: 20),
+              roundedContainer(Colors.pinkAccent),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget roundedContainer(Color color) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+    );
+  }
+
+ _onTap() {
+    setState(() {
+      _bottomSheetBottomPosition = isCollapsed
+          ? widget._expandedBottomSheetBottom
+          : widget._collapsedBottomSheetBottom;
+      isCollapsed = !isCollapsed;
+    });
+  }
+
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        isCollapsed = true;
+        _bottomSheetBottomPosition = widget._collapsedBottomSheetBottom;
+      });
+    });
   }
 }
