a08e936b8af30462cad88989357574f36713d906
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2020-04-14 14:10:13 +0100

    Added more characters

--- lib/models/characters.dart
+++ lib/models/characters.dart
@@ -24,5 +24,26 @@ List characters = [
     "Agnes Gru it is one of Gru and Lucy's three adopted daughters, alongside her sisters Margo and Edith. She is the youngest child of the three sisters. She greatly adores unicorns, as shown on various occasions. Agnes is a little girl with dark brown eyes. Her long black hair is tied in an upwards ponytail with a red scrunchie. Most of the time, Agnes wears blue overalls over a yellow and brown striped t-shirt, and white sneakers with yellow socks. She also wears a white ballet outfit like Edith and Margo (at the ballet recital). For pajamas, Agnes wears a long blue nightshirt covered with teddy bears and polar bear slippers; her hair stays the same. On her birthday, Agnes is wearing a dress that resembles a princess riding a unicorn. The colors are similar to her regular outfit. She also wears a blue princess hat on her head.",
       colors: [Colors.pink.shade200, Colors.redAccent.shade400]
   ),
+  Character(
+    name: "Lucy",
+    imagePath: "assets/images/agent.png",
+    description:
+    "Lucy Wilde-Gruwas a rookie agent of the Anti-Villain League. She is the love interest, later wife of Felonius Gru and adoptive mother to Margo, Edith, and Agnes.",
+      colors: [Colors.teal.shade200, Colors.tealAccent.shade400]
+  ),
+  Character(
+    name: "Margo",
+    imagePath: "assets/images/margo.png",
+    description:
+    "Margo is the oldest of the three orphan girls that Gru adopts. Before she was adopted, she was an orphan horribly treated by Miss Hattie. Like Edith and Agnes (Margo's younger sisters), Margo wished to be adopted by somebody who would love them. She is very disapproving of Edith's mischief, shown where Edith jumps in puddles which gets her and Agnes wet, and also when she breaks a vial of acid in Gru's kitchen.",
+      colors: [Colors.brown.shade200, Colors.brown.shade400]
+  ),
+  Character(
+    name: "Gru",
+    imagePath: "assets/images/gru.png",
+    description:
+    "Gru comes from a long lineage of villain as shown with dozen of statues of his ancestors in Dru's hideout. Although his hometown is unknown, in an interview with Ellen DeGeneres he claimed he was from Albuquerque, New Mexico.As a child he had always dreamed of going to the moon. He made a prototype rocket out of macaroni and an actual working prototype rocket. For most of his childhood he was neglected and ignored by his mother, and it is unknown if this is what inspired him to become a supervillain, or if the Gru family is a family of supervillains, although his mother showed no signs of being one. He has worked with Dr. Nefario, who builds gadgets and weapons for Gru, for some time and based on what Dr. Nefario said, they have been planning the moon heist for a long time.",
+      colors: [Colors.grey.shade200, Colors.grey.shade400]
+  ),
 ];
 
